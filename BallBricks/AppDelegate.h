c32e49b12395a36674dc1bda6462d8d2f0ec95f5
Anatoly Kovalev <inatoley@gmail.com>
2015-09-01 16:39:43 +0300

    Just created another project

---
+++ BallBricks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BallBricks
+//
+//  Created by Fenkins on 01/09/15.
+//  Copyright (c) 2015 Fenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
